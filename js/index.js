83a5a4f7244174f01bf48a133bf03fa2ebed9fe4
Noelia <noeliariega@gmail.com>
2024-02-15 19:15:28 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -60,32 +60,6 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
-
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-});
-
-// Coding Challenge #1
-
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
-
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-*/
-
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number'), (textContent = '?');
-  document.querySelector('.guess').value = '';
-
-  document.querySelector('.body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
